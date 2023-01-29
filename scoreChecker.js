abfc361589732e549f9255f4c2971dbebfb01272
ilmialimudins <ilmi.muhammad@outlook.com>
2023-01-30 06:44:58 +0700

    first

---
+++ scoreChecker.js
@@ -0,0 +1,21 @@
+function scoreChecker(score) {
+    let result;
+  
+    // TODO
+  if(score >=90){
+  console.log('Selamat! Anda mendapatkan nilai A.');
+  }else if(score = 80-89){
+    console.log('Anda mendapatkan nilai B.');
+  }else if(score =70-79){
+    console.log('Anda mendapatkan nilai C.');
+  }else if(score =6-79){
+    console.log('Anda mendapatkan nilai D.');
+  }else {
+    console.log('Anda mendapatkan nilai E.');
+  
+  
+//     // Jangan hapus kode ini
+    return result;
+  }} (scoreChecker());
+
+
